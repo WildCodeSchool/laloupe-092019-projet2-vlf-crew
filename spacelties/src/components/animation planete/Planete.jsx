327fd7b70355beea75a0009caa481da09dd51237
Fabien <fabienfiette@hotmail.fr>
2019-11-08 09:26:10 +0100

    save

---
+++ spacelties/src/components/animation planete/Planete.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./planete.css";
+import PlanetList from './PlanetList';
+
+
+
+class Planete extends React.Component {
+    render() {
+        return (
+
+            <div>
+                <PlanetList />
+            </div>
+
+        )
+    }
+}
+
+export default Planete;
